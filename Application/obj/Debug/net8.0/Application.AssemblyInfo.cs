20bb227e494d9bf9422a6552d718b06ae95ad08a
Jorge Ponce <91036361+Thealaskage@users.noreply.github.com>
2025-06-05 23:18:09 -0500

    feat(security): merge feature/advance-security-controller into develop

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6e18ce8bc33e430261d4cf678ba57211e17e64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2639798ca9d31cdcb5e5e8a2e14682d434d87d5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
