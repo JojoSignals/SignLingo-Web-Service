9bc206b640c1436b276d4159978df3e2adfc9afb
DiegoR434 <u202123548@upc.edu.pe>
2025-06-18 16:24:33 -0500

    feat: added Unit application layer files

--- Application/Shared/Mapping/ModelToResponse.cs
+++ Application/Shared/Mapping/ModelToResponse.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Domain.ExercisesManager.Model.Aggregates;
 using Domain.ExercisesManager.Model.Commands;
+using Domain.ExercisesManager.Model.Entities;
 using Domain.ExercisesManager.Model.Responses;
 using Domain.Security.Model.Commands;
 using Domain.Security.Model.Entities;
@@ -20,7 +21,13 @@ public class ModelToResponse : Profile
         // Exercises Manager
         CreateMap<Exercise, ExerciseResponse>();
         CreateMap<EditExerciseCommand, ExerciseResponse>();
-        CreateMap<DeleteExerciseCommand, ExerciseResponse>();
         CreateMap<CreateExerciseCommand, ExerciseResponse>();
+        CreateMap<DeleteExerciseCommand, ExerciseResponse>();
+
+        CreateMap<Unit, UnitResponse>();
+        CreateMap<EditUnitCommand, UnitResponse>();
+        CreateMap<CreateUnitCommand, UnitResponse>();
+        CreateMap<DeleteUnitCommand, UnitResponse>();
+
     }
 }
