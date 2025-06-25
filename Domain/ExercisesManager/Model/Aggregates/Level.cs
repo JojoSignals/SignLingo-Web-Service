e9ee1addfbe157bcc734fd1d45875fe049ff40e3
DiegoR434 <u202123548@upc.edu.pe>
2025-06-25 10:20:56 -0500

    feat: added Domain layer for Levels

---
+++ Domain/ExercisesManager/Model/Aggregates/Level.cs
@@ -0,0 +1,17 @@
+using Domain.ExercisesManager.Model.Entities;
+using Domain.Shared;
+
+namespace Domain.ExercisesManager.Model.Aggregates;
+
+public class Level : BaseModel
+{
+    public string LevelName { get; set; }
+    public string LevelDescription { get; set; }
+    public int ExperienceRequiered { get; set; }
+    public int TotalQuestions { get; set; }
+    public Unit Unit { get; set; }
+    public Icon Icon { get; set; }
+    
+    public int UnitId { get; set; }
+    public int IconId { get; set; }
+}
