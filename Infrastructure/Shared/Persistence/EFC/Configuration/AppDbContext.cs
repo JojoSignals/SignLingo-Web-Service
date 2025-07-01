4a6ca5d470c189e59401324f51dfdbac0dded263
DiegoR434 <u202123548@upc.edu.pe>
2025-06-30 22:39:49 -0500

    fix: Icon relation on Db

--- Infrastructure/Shared/Persistence/EFC/Configuration/AppDbContext.cs
+++ Infrastructure/Shared/Persistence/EFC/Configuration/AppDbContext.cs
@@ -124,8 +124,8 @@ public class AppDbContext : DbContext
         //Level --- Icon
         builder.Entity<Level>()
             .HasOne(l => l.Icon)
-            .WithOne(i => i.Level)
-            .HasForeignKey<Level>(l => l.IconId);
+            .WithMany(i => i.Levels)
+            .HasForeignKey(l => l.IconId);
         
   
 
